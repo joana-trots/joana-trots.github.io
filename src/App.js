81ca5b6628714203ac2abbc2be72f9832569f228
Yana Trots <jana.trots@gmail.com>
2022-12-23 00:07:44 +0100

    upd

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.sass'
 import { Routes, Route } from 'react-router-dom'
-import Layout from './components/layout/'
+import Layout from './components/layout'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
-import Portfolio from './components/Portfolio/'
+import Portfolio from './components/Portfolio'
 
 function App() {  
   return (
    <Routes>
     <Route path="/" element={<Layout />} >
-      <Route index element={<Home />}></Route>
+      <Route index path='/' element={<Home />}></Route>
       <Route path='about' element={<About />}></Route>
       <Route path='contact' element={<Contact />}></Route>
       <Route path='my-work' element={<Portfolio />}></Route>
